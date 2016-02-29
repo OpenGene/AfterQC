4ea1dac9774e61ad7a4945842aebd71efcaa2a31
Shifu Chen <chen@haplox.com>
2016-02-29 21:53:09 +0800

    add overlap function

--- util.py
+++ util.py
@@ -35,3 +35,29 @@ def editDistance(s1, s2):
             tbl[i][j] = min(tbl[i][j-1]+1, tbl[i-1][j]+1, tbl[i-1][j-1]+cost)
 
     return tbl[i][j]
+
+def distance_threshold(overlap_len):
+    return min(6, overlap_len/10.0)
+
+def overlap(r1, r2):
+    len1 = len(r1)
+    len2 = len(r2)
+    reverse_r2 = reverseComplement(r2)
+
+    # a match of less than 5 is considered as unconfident
+    for offset in range(0,len1-5):
+        # the overlap length of r1 & r2 when r2 is move right for offset
+        overlap_len = min(len1, (offset + len2 )) - offset
+
+        # remind that Julia is a 1-based coordination system
+        distance = editDistance(r1[offset : offset+overlap_len], reverse_r2[0 : overlap_len])
+        if distance <= distance_threshold(overlap_len):
+            # now we find a good candidate
+            # we verify it by moving r2 one more base to see if the distance is getting longer
+            # if yes, then current is the best match, otherwise it's not
+            next = offset + 1
+            next_overlap_len = min(len1, (next + len2 )) - next
+            distance2 = editDistance(r1[next : next+next_overlap_len], reverse_r2[0 : next_overlap_len])
+            if distance <= distance2:
+                return (offset, overlap_len, distance)
+    return (0,0,0)
