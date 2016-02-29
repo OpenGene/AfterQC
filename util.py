3f9a5cd34af14dc2c2109530adca77c737cf21c1
Shifu Chen <chen@haplox.com>
2016-02-29 15:21:09 +0800

    switch to using list instead of dict for edit distance

--- util.py
+++ util.py
@@ -26,12 +26,12 @@ def editDistance(s1, s2):
     m=len(s1)+1
     n=len(s2)+1
 
-    tbl = {}
-    for i in xrange(m): tbl[i,0]=i
-    for j in xrange(n): tbl[0,j]=j
+    tbl = [([0] * n) for i in xrange(m)]
+    for i in xrange(m):tbl[i][0]=i
+    for j in xrange(n):tbl[0][j]=j
     for i in xrange(1, m):
         for j in xrange(1, n):
             cost = 0 if s1[i-1] == s2[j-1] else 1
-            tbl[i,j] = min(tbl[i, j-1]+1, tbl[i-1, j]+1, tbl[i-1, j-1]+cost)
+            tbl[i][j] = min(tbl[i][j-1]+1, tbl[i-1][j]+1, tbl[i-1][j-1]+cost)
 
-    return tbl[i,j]
+    return tbl[i][j]
